2cb7ed774ea4b7397bb6ec6cb4d9e7188d7b2d8f
ArnY3 <86603335+ArnY3@users.noreply.github.com>
2021-09-08 08:36:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+
+
+function preload(){
+  //pre-load images
+  pathImg = loadImage("path.png");
+  jaxonAnimation = loadAnimation = ("Runner-1.png","Runner-2.png");
+}
+
+function setup(){
+  createCanvas(400,400);
+  //create sprites here
+   jaxon = createSprite ();
+   jaxon.addAnimation (jaxonAnimation);
+
+   path = createSprite();
+   path.addImage = (pathImg);
+   path.velocityY = 4;
+   path.scale = 1.2;
+  
+}
+
+function draw() {
+  background(0);
+ 
+  if (path.y > 400) {
+   
+    path.y = height/2;
+
+  }
+   }
+    
